225df0e99230e988b725bf1ed4685b46826300b1
Matt Dickenson <mcdickenson@gmail.com>
2013-02-15 19:10:04 -0500

    implemented part 1 of Markov assignment

--- Assignments/3/compsci201_Markov/src/MarkovMain.java
+++ Assignments/3/compsci201_Markov/src/MarkovMain.java
@@ -6,7 +6,7 @@
  */
 public class MarkovMain {
     public static void main(String[] args){
-        IModel model = new MarkovModel();
+        IModel model = new MapMarkovModel();
         SimpleViewer view = new SimpleViewer("Compsci 100 Markov Generation", "k count>");
         view.setModel(model);
     }
