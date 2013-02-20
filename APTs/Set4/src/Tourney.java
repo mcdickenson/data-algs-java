b46cf3cb274f1abb531d677f1f60a79e7f8ddcf8
Matt Dickenson <mcdickenson@gmail.com>
2013-02-20 16:12:01 -0500

    completed APT set 4 Tourney challenge - passes all tests

--- APTs/Set4/src/Tourney.java
+++ APTs/Set4/src/Tourney.java
@@ -2,8 +2,6 @@ import java.lang.Math;
 
 public class Tourney {
       public String winner(String[] bracket, String results) {
-           // fill in code here
-    	  
     	  int numGames = results.length(); 
     	  int numTeams = bracket.length; 
     	  if(numTeams==1){ return bracket[0]; }
@@ -12,9 +10,7 @@ public class Tourney {
     	  String result; 
     	  for(int i=0; i<numTeams; i+=2){
     		  String team1 = bracket[i];
-//    		  System.out.println(team1);
     		  String team2 = bracket[i+1];
-//    		  System.out.println(team2);
     		  if(team1.equals("bye")){ victor = team2; }
     		  else if(team2.equals("bye")){ victor = team1; }
     		  else{
@@ -24,21 +20,6 @@ public class Tourney {
     			  else if(result.equals("H")){ victor = team1; }
     		  }
     		  nextRound[i/2] = victor; 
-//    		  System.out.println("winner: " + victor); 
-//    		  System.out.println("results: " + results );
-    	  }
-//    	  System.out.println("next round:");
-//    	  for(String team : nextRound){
-////    		  System.out.println(team);
-//    	  }
     	  return winner(nextRound, results); 
       }
-      
-//      public static void main(String[] args){
-//    	  Tourney t = new Tourney(); 
-//    	  String[] bracket = {"A","B","C","bye","D","E","F","bye"}; 
-//    	  String results = "LHHLH"; 
-//    	  String victor = t.winner(bracket, results);
-//    	  System.out.println(victor); 
-//      }
 }
