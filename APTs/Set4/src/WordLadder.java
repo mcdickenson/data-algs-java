f9e32eca2a1cb652947b30880e5d6e90b66b0355
Matt Dickenson <mcdickenson@gmail.com>
2013-02-20 11:48:37 -0500

    started APT set 4

---
+++ APTs/Set4/src/WordLadder.java
@@ -0,0 +1,9 @@
+
+ public class WordLadder {
+    public String ladderExists(String[] words, 
+                               String from, String to) {
+        if(from.equals(to)){ return "ladder"; } 
+    	
+    	return "";
+    }
+  }
