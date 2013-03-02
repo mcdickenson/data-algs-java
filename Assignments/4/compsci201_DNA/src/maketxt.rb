9e47a65b3325e78ceda009f7da1632cd7e166efc
Matt Dickenson <mcdickenson@gmail.com>
2013-03-02 11:31:52 -0500

    make ruby script more flexible to clean either file

--- Assignments/4/compsci201_DNA/src/maketxt.rb
+++ Assignments/4/compsci201_DNA/src/maketxt.rb
@@ -1,9 +1,13 @@
 # convert .dat file to .txt 
 
-# read = 
-write = File.open('../data/ecoli_small_clean.txt', 'w')
+# writename = '../data/ecoli_small_clean.txt'
+writename = '../data/ecoli_clean.txt'
+write = File.open(writename, 'w')
 
-File.open('../data/ecoli_small.txt', 'r') do |f1|
+# filename = '../data/ecoli_small.txt'
+filename = '../data/ecoli.txt'
+
+File.open(filename, 'r') do |f1|
 	while line = f1.gets
 		clean = line.gsub(/\ \d+/, '').gsub("\n", '').gsub("\t", '')
 		write.puts clean.squeeze(' ')
