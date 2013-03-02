8a3901d8fb1cc49eca7ec8e27067577d17347c45
Matt Dickenson <mcdickenson@gmail.com>
2013-03-02 14:11:43 -0500

    added R code to plot benchmark and overflow run times for SimpleStrand

---
+++ Assignments/4/compsci201_DNA/src/dna.R
@@ -0,0 +1,60 @@
+# Compsci 201, Spring 2013, Duke University
+# Assignment 4: DNA
+# Matt Dickenson
+
+# load data
+pathData = '~/desktop/data-algs-java/Assignments/4/compsci201_DNA/data'
+pathWriteup = '~/desktop/data-algs-java/Assignments/4/compsci201_DNA/writeup'
+setwd(pathData)
+
+default = read.csv('runtimes.csv')
+m512 = read.csv('runtimes512.csv')
+m1024 = read.csv('runtimes1024.csv')
+dim(default)
+dim(m512)
+dim(m1024)
+
+# explore
+head(default)
+names(default)
+plot(default$splicee, default$time1)
+
+default$best <- apply(default[,6:8], 1, min)
+default$worst <- apply(default[,6:8], 1, max)
+default$avg <- apply(default[,6:8], 1, mean)
+
+plot(default$recomb, default$avg, type='o',
+	xaxt='n', xlab="    N", ylab="Time (secs)")
+axis(side=1, las=2, at=default$recomb, labels=as.character(default$recomb))
+
+# function to plot runtimes vs input
+plottimes <- function(data, mainlab){
+	data$best <- apply(data[,6:8], 1, min)
+	data$worst <- apply(data[,6:8], 1, max)
+	data$avg <- apply(data[,6:8], 1, mean)
+	plot(data$recomb, data$avg, type='o',
+		ylim=c(min(data$best)*0.9,max(data$worst*1.1)), 
+		xaxt='n', 
+		xlab="", ylab="Time (secs)",
+		main=mainlab)
+	axis(side=1, las=2, at=data$recomb, labels=as.character(data$recomb))
+	lines(data$recomb, data$best, type='o', pch=5, lty=2)
+	lines(data$recomb, data$worst, type='o', pch=0, lty=2)
+	legend('bottomright', legend=c('average', 'best', 'worst'), pch=c(1,5,0))
+}
+
+setwd(pathWriteup)
+
+	plottimes(default, "SimpleStrand Runtime as\nFunction of Recombinations - Default Mem")
+
+pdf('default.pdf')
+	plottimes(default, "SimpleStrand Runtime as\nFunction of Recombinations - Default Mem")
+dev.off()
+
+pdf('m512.pdf')
+	plottimes(m512, "SimpleStrand Runtime as\nFunction of Recombinations - 512 Mem")
+dev.off()
+
+pdf('m1024.pdf')
+	plottimes(m1024, "SimpleStrand Runtime as\nFunction of Recombinations - 1024 Mem")
+dev.off()
