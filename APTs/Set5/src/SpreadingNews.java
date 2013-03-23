f83c370f775ad10acb88217371901cd465590dec
Matt Dickenson <mcdickenson@gmail.com>
2013-03-23 09:19:34 -0400

    updated apt set 5 SpreadingNews challenge - passes 7/80 tests

--- APTs/Set5/src/SpreadingNews.java
+++ APTs/Set5/src/SpreadingNews.java
@@ -1,99 +1,101 @@
 import java.util.HashMap;
 
 public class SpreadingNews {
-     public int minTime(int[] supervisors) {
-    	 Node myRoot = new Node(0, null);
-    	 HashMap<Integer, Node> map = new HashMap<Integer, Node>();
-    	 map.put(0, myRoot);
-    	 
-    	 // add all employees to tree
-    	 for(Integer i=1; i<supervisors.length; i++){
-    		 Node boss = map.get(supervisors[i]);
-    		 Node newNode = new Node(i, boss);
-    		 boss.add(newNode);
-    		 map.put(i, newNode);
-    	 }
-    	 
-    	 int minutes = 0; 
-    	 boolean[] called = new boolean[supervisors.length]; 
-    	 called[0] = true; 
-    	 for(int i=1; i<supervisors.length; i++){
-    		 called[i] = false; 
-    	 }
-    	 boolean allCalled = false;
-    	 
-    	 
-    	 // go through map
-    	 while(!allCalled & minutes<supervisors.length){
-    		 for(Integer i: map.keySet()){
-    			 if(called[i]){
-    				 Node boss = map.get(i); // anyone who has been called so far
-        			 int max2away = -1; 
-        			 int idMax=-1;
-        			 for(Integer empid : boss.myMap.keySet()){
-        				 Node tmp = boss.myMap.get(empid);
-        				 if(!called[empid]){
-        					 int num2away = tmp.myMap.size();
-            				 if(num2away > max2away){ idMax = empid; }
-        				 }
-        			 }
-        			 // boss calls employee with most subordinates
-        			 if(idMax > 0){
-        				 Node one2call = boss.myMap.get(idMax);
-        				 one2call.isCalled = true; 
-        				 called[idMax] = true; 
-        			 }
-    			 }
-    			 
-    		 }
-    		 allCalled = checkCalled(called);
-    		 minutes++; 
-    	 }
-    	 // for a node for which index < calls+1:
-    	 // look at its children
-    	 // whichever one has most employees it calls first
-    	 // everyone who has been called can make calls
-    	 
-    	 return minutes; 
-     }
+	public int minTime(int[] supervisors) {
+   	 
+		int minutes=0;
+		HashMap<String, String> relationships = new HashMap<String, String>();
+		String called = Character.toString((char) 65);
+		String hasBeenCalled = called; 
+		
+		for(int i=1; i<supervisors.length; i++){
+//			int bossInt = supervisors[i];
+			String bString = Character.toString((char) (supervisors[i]+65));
+//			System.out.println(bString);
+			String eString = Character.toString((char) (i+65));
+//			System.out.println(eString);
+			// if it's already in there, get current string, append this one, put back
+			if(relationships.containsKey(bString)){
+				String current = relationships.get(bString);
+				current = current + eString;
+				relationships.put(bString, current);
+			}
+			// if not already in there, put this one in
+			else{
+				relationships.put(bString, eString);
+			}
+		}
+//		System.out.println(relationships.size());
+//		System.out.println(relationships.get("A"));
+		
+		//whileloop
+		while(called.length()<supervisors.length & minutes<supervisors.length){
+			System.out.printf("minutes %d %n", minutes);
+			
+			//iterate over relationships.keySet
+			for(String key: relationships.keySet()){
+				System.out.println("key "+key);
+				
+				if(hasBeenCalled.contains(key)){ //if that person has been called
+					String values = relationships.get(key);
+					System.out.println("values "+values);
+					//find which of their mapped values has the most values mapped to it
+					int max = -1;
+					int howManySubords;
+					String hasMost = "";
+					for(int j=0; j<values.length(); j++){
+						String employee = values.substring(j,j+1);
+						if(relationships.containsKey(employee)){
+							String subords = relationships.get(employee);
+							howManySubords = subords.length();
+						}
+						else{ howManySubords=0; }
+						if(howManySubords>max){ hasMost = employee; }
+					}
+					System.out.println("has most: "+hasMost);
+					
+					// remove hasMost from values
+					values = values.replaceAll(hasMost, "");
+					System.out.println("values updated "+values);
+					relationships.put(key, values);
+					
+					// add hasMost to called
+					called = called + hasMost; 
+					System.out.println("called " + called);
+					
+					// if size of values is zero, remove this key from relationships
+//					if(values.length()==0){
+//						relationships.remove(key);
+//					}
+					
+				}
+			}
+			minutes++;
+			hasBeenCalled = called;
+		}
+		
+		
+
+
+		return minutes; 
+    }
+	
+	public boolean allCalled(boolean[] called){
+		for(int i=1; i<called.length; i++){
+			if(called[i]==false){ return false;}
+		}
+		return true; 
+	}
      
-     public boolean checkCalled(boolean[] called){
-    	 for(int i=0; i<called.length; i++){
-    		 if(called[i]==false){ 
-//    			 System.out.println(i);
-    			 return false;
-    		 }
-    	 }
-    	 return true; 
-     }
-     
-     
-     public class Node{
-    	 Integer myData;
-    	 boolean isCalled;
-    	 Node myBoss;
-    	 HashMap<Integer, Node> myMap = new HashMap<Integer, Node>();
-    	 
-    	 public Node(Integer data, Node boss){
-    		 myData = data;
-    		 isCalled = false;
-    		 myBoss = boss; 
-    	 }
-    	 
-    	 public void add(Node n){
-    		 myMap.put(n.myData, n);
-    	 }
-     }
-     
-     public static void main(String[] args){
-    	 SpreadingNews sn = new SpreadingNews();
-    	 int[] test1 = {-1, 0, 0};
-    	 int result1 = sn.minTime(test1);
-    	 System.out.println(result1);
-    	 
-    	 int[] test2 = {-1, 0, 0, 2, 2};
-    	 int result2 = sn.minTime(test2);
-    	 System.out.println(result2);
-     }
+//     public static void main(String[] args){
+//    	 SpreadingNews sn = new SpreadingNews();
+//    	 int[] test1 = {-1, 0, 0};
+//    	 int result1 = sn.minTime(test1);
+//    	 System.out.println(result1);
+//    	 
+//    	 int[] test2 = {-1, 0, 0, 2, 2};
+//    	 int result2 = sn.minTime(test2);
+//    	 System.out.println(result2);
+//     }
 
 }
