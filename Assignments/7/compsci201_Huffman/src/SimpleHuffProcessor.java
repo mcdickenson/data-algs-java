7b7579413f632fb2fa61a50fa7d319dd7beea508
Matt Dickenson <mcdickenson@gmail.com>
2013-04-14 14:14:51 -0400

    began implementing compression step

--- Assignments/7/compsci201_Huffman/src/SimpleHuffProcessor.java
+++ Assignments/7/compsci201_Huffman/src/SimpleHuffProcessor.java
@@ -3,7 +3,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.PriorityQueue;
-//import java.util.HashSet;
 
 public class SimpleHuffProcessor implements IHuffProcessor {
     
@@ -13,11 +12,30 @@ public class SimpleHuffProcessor implements IHuffProcessor {
     private Integer mySize; 
     
     public int compress(InputStream in, OutputStream out, boolean force) throws IOException {
-        throw new IOException("compress is not implemented");
-        //return 0;
-        // read in 8-bit chunks
-        // for each chunk, qrite the encoding from the map
-        // use a BitOutputStream 
+    	// write the magic number
+    	BitOutputStream bout = new BitOutputStream(out); 
+    	bout.writeBits(BITS_PER_INT, MAGIC_NUMBER); 
+    	
+    	// write info that allows tree to be recreated
+    	writeTraversal(myRoot, bout); 
+    	
+    	// write bits needed to encode each character of input file 
+    	BitInputStream binput = new BitInputStream(in);
+        int next = 1;
+        while(next > 0){
+        	next = binput.read(); 
+        	String encoding = myMap.get(next);
+        	for(int i=0; i<encoding.length(); i++){
+        		char c = encoding.charAt(i);
+        		if(c=='0'){ bout.writeBits(1, 0);}
+        		else if(c=='1'){ bout.writeBits(1, 1); } 
+        	}
+        }
+        bout.writeBits(BITS_PER_INT, PSEUDO_EOF);
+        binput.close(); 
+    	bout.close(); 
+    	
+    	return 0; 
     }
 
 
@@ -78,6 +96,18 @@ public class SimpleHuffProcessor implements IHuffProcessor {
     		encodePaths(t.myRight, path + "1");
     	}
     }
+    
+    public void writeTraversal(TreeNode t, BitOutputStream out){
+    	if(t.isLeaf()){
+    		out.writeBits(BITS_PER_INT, 1);
+    		out.writeBits(BITS_PER_INT, t.myValue);
+    	}
+    	else{
+    		out.writeBits(BITS_PER_INT, 0);
+    		writeTraversal(t.myLeft, out);
+    		writeTraversal(t.myRight, out); 
+    	}
+    }
 
     public void setViewer(HuffViewer viewer) {
         myViewer = viewer;
@@ -86,6 +116,11 @@ public class SimpleHuffProcessor implements IHuffProcessor {
     public int uncompress(InputStream in, OutputStream out) throws IOException {
         throw new IOException("uncompress not implemented");
         //return 0;
+        
+//        int magic = in.readBits(BITS_PER_INT);
+//        if (magic != MAGIC_NUMBER){
+//            throw new IOException("magic number not right");
+//        }
     }
     
     private void showString(String s){
