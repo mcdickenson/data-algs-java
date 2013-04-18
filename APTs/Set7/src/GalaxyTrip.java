11823592c9166ffe69fe8382696c60ae31846ff9
Matt Dickenson <mcdickenson@gmail.com>
2013-04-18 16:13:22 -0400

    started trying to solve summation problem using bits

--- APTs/Set7/src/GalaxyTrip.java
+++ APTs/Set7/src/GalaxyTrip.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+//import java.util.BitSet;
+//import java.util.BitSet.*;
 import java.util.Collections;
 import java.util.HashMap;
 
@@ -44,10 +46,10 @@ public class GalaxyTrip {
 //    	   for(int size: sizes){
 //    		   System.out.println(size);
 //    	   }
-//    	   System.out.println(sizes);
     	   
     	   ArrayList<Integer> combo = new ArrayList<Integer>();
-    	   combo = sumArray(sizes.length, sizes, combo);
+    	   sumArray(sizes);
+//    	   combo = sumArray(sizes.length, sizes, combo);
     	   
     	   Collections.sort(combo);
     	   int[] result = buildIntArray(combo);
@@ -59,44 +61,112 @@ public class GalaxyTrip {
     	   return result; 
        }
        
-       public ArrayList<Integer> sumArray(int windowSize, int[] sizes, ArrayList<Integer> combo){
-//    	   System.out.print("window: ");
-//    	   System.out.println(windowSize);
-    	   if(windowSize==1){
-    		   for(int i=0; i<sizes.length; i++){
-    			   int result = sizes[i];
-    			   if(!combo.contains(result)){
-    				   combo.add(result);
-    			   }
-    		   }
-//    		   System.out.print("size: ");
-//    		   System.out.println(combo.size());
-    		   return combo;
-    	   }
-    	   else{
-    		   ArrayList<Integer> tmp = sumArray(windowSize-1, sizes, combo);
-    		   
-    		   int upperlimit = tmp.size();
-    		   if(upperlimit>1){
-    			   for(int i=1; i<upperlimit; i++){
-        			   int v1 = tmp.get(i-1);
-        			   int v2 = tmp.get(i);
-        			   int total = v1+v2;
-        			   if(!tmp.contains(total)){
-        				   tmp.add(total);
-        			   }
-        		   }
-    		   }
-    		   else{
-    			   int total = tmp.get(0)*2;
-    			   tmp.add(total);
-    		   }
-    		   
-//    		   System.out.print("size: ");
-//    		   System.out.println(tmp.size());
-    		   return tmp;
-    	   }
+       public void sumArray(int[] input){
+    	    int size = (int)(Math.pow(2.0, input.length)) - 1;
+    	    int[] output = new int[size];
+    	    for (int i = 1; i < output.length; i++) { // maybe leq
+
+//    	        BitSet bs = BitSet.valueOf(new long[]{i});
+    	    	String iBinary = Integer.toBinaryString(i);
+//    	    	char iFirst = iBinary.charAt(0);
+    	    	
+    	    	System.out.print("i: ");
+    	    	System.out.println(iBinary);
+    	        int sum = 0;
+    	        for (int j = 0; j < i; j++) { // maybe leq; maybe j<size
+    	        	String jBinary = Integer.toBinaryString(j);
+    	        	System.out.print("j: ");
+    	        	System.out.println(jBinary);
+//    	        	char jLast = jBinary.charAt(jBinary.length()-1);
+//    	        	char iLast = iBinary.charAt(iBinary.length()-1);
+//    	        	char jFirst = jBinary.charAt(j);
+//    	        	char iLast = iBinary.charAt(0);
+    	        	
+    	        	char jCurrent = jBinary.charAt(j);
+    	        	char iCurrent = iBinary.charAt(j);
+    	        	
+    	            if (jCurrent!=iCurrent) {
+//    	            	System.out.println(j);
+    	                sum += input[j];
+    	            }
+    	        }
+    	        output[i-1] = sum;
+    	        System.out.print("output: ");
+    	        System.out.println(sum);
+    	    }
+    	    for(int o: output){
+    	    	System.out.println(o);
+    	    }
+
        }
+       
+       
+//       public ArrayList<Integer> sumArray(int windowSize, int[] sizes, ArrayList<Integer> combo){
+////    	   System.out.print("window: ");
+////    	   System.out.println(windowSize);
+//    	   
+////    	   if(windowSize==1){
+//    		   for(int i=0; i<sizes.length; i++){
+//    			   int result = sizes[i];
+//    			   if(!combo.contains(result)){
+//    				   combo.add(result);
+//    				   System.out.println(result);
+//    			   }
+////    			   for(int j=0; j<combo.size()-1; j++){
+////    				   int total = result + combo.get(j);
+////    				   System.out.println(total);
+////    				   if(!combo.contains(total)){
+////        				   combo.add(total);
+////        			   }
+////    			   }
+//    		   }
+////    		   System.out.print("size: ");
+////    		   System.out.println(combo.size());
+//    		   
+////    	   }
+//    		   
+//    		   int first = 0;
+//    		   int second = 1;
+//    		   while(second<combo.size()-1){	   
+//    			   second = first+1;
+//    			   
+////    			   int v1 = combo.get(first);
+////    			   int v2 = combo.get(second);
+////    			   int total = v1+v2;
+////    			   if(!combo.contains(total)){
+////    				   combo.add(total);
+////    			   }
+////    			   first++;
+//    			   second = first+1;
+//    		   }
+//    		   
+//    		   
+//    		   return combo;
+    	   
+//    	   else{
+//    		   ArrayList<Integer> tmp = sumArray(windowSize-1, sizes, combo);
+//    		   
+//    		   int upperlimit = tmp.size();
+//    		   if(upperlimit>1){
+//    			   for(int i=1; i<upperlimit; i++){
+//        			   int v1 = tmp.get(i-1);
+//        			   int v2 = tmp.get(i);
+//        			   int total = v1+v2;
+//        			   if(!tmp.contains(total)){
+//        				   tmp.add(total);
+//        			   }
+//        		   }
+//    		   }
+//    		   else{
+//    			   int total = tmp.get(0)*2;
+//    			   tmp.add(total);
+//    		   }
+//    		   
+////    		   System.out.print("size: ");
+////    		   System.out.println(tmp.size());
+//    		   return tmp;
+//    	   }
+//       }
        
        public int[] buildIntArray(ArrayList<Integer> integers) {
     	    int[] ints = new int[integers.size()];
@@ -107,13 +177,13 @@ public class GalaxyTrip {
     	    return ints;
     	}
        
-//       public static void main(String[] args){
-//    	   GalaxyTrip gt = new GalaxyTrip();
-//    	   
-//    	   String[] d1 = {"1 2", "0", "0", ""};
-//    	   int[] test1 = {1, 3, 4};
-//    	   int[] result1 = gt.possibleValues(d1);
-//    	   if(test1.length==result1.length){ System.out.println("test 1 passes");}
-//    	   else{System.out.println("test 1 fails");}
-//       }
+       public static void main(String[] args){
+    	   GalaxyTrip gt = new GalaxyTrip();
+    	   
+    	   String[] d1 = {"1 2", "0", "0", ""};
+    	   int[] test1 = {1, 3, 4};
+    	   int[] result1 = gt.possibleValues(d1);
+    	   if(test1.length==result1.length){ System.out.println("test 1 passes");}
+    	   else{System.out.println("test 1 fails");}
+       }
 }
