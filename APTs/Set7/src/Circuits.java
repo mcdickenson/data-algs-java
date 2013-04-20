9bda70de3a8a71b7674b96337235cedcbe5c2dcb
Matt Dickenson <mcdickenson@gmail.com>
2013-04-20 13:16:23 -0400

    Circuits passes all tests

--- APTs/Set7/src/Circuits.java
+++ APTs/Set7/src/Circuits.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 
 public class Circuits {
 	
@@ -15,53 +14,17 @@ public class Circuits {
 			}
 		}
 		
-//		for(int[] row : myGraph){
-//			for(int col: row){
-//				System.out.print(col);
-//				System.out.print(",");
-//			}
-//			System.out.println("");
-//		}
-		
-		
-//		int[] longestPaths = new int[connects.length];
-//		for(int i=0; i<connects.length; i++){
-//			ArrayList<Integer> visited = new ArrayList<Integer>();
-//			longestPaths[i] = longestFromMe(myGraph, i, 0, visited);
-//		}
-		
-//		return rowMax(longestPaths);
-		boolean[] visited = new boolean[connects.length];
+		int[] longestPaths = new int[connects.length];
 		for(int i=0; i<connects.length; i++){
-			visited[i] = false;
-		}
-		
-		return longestPath(0, visited, myGraph);
-//		return 0;
-	}
-	
-	public int longestFromMe(int[][] graph, int start, int pathLength, ArrayList<Integer> visited){
-		
-		int[] row = graph[start];	
-		
-		if(maxCol(row)==-1) { return pathLength; }
-		else{
-			visited.add(start);
-			int[] maxPaths = new int[row.length];
-			for(int i=0; i<row.length; i++){
-				
-				if(!visited.contains(i)){
-					maxPaths[i] = longestFromMe(graph, i, row[i], visited);
-				}
-				
+			boolean[] visited = new boolean[connects.length];
+			for(int j=0; j<connects.length; j++){
+				visited[j] = false;
 			}
-			pathLength += rowMax(maxPaths);
-//			+row[maxCol(maxPaths)];
-			
-			return pathLength; 
+			longestPaths[i] = longestPath(i, visited, myGraph);
 		}
 		
-	}
+		return rowMax(longestPaths);
+	}	
 	
 	public int longestPath(int start, boolean[] visited, int[][] graph){
 		int dist = 0;
@@ -84,7 +47,6 @@ public class Circuits {
 		return max;
 	}
 	
-	
 	public int rowMax(int[] row){
 		int max = 0;
 		for(int i=0; i<row.length; i++){
@@ -93,24 +55,13 @@ public class Circuits {
 		return max;
 	}
 	
-	public int maxCol(int[] row){
-		int biggestCol = -1;
-		int max = 0;
-		for(int i=0; i<row.length; i++){
-			if(row[i]>max){ biggestCol = i; }
-		}
-		return biggestCol;
-	}
-	
-	
-	public static void main(String[] args){
-		String[] dependencies = {"1 2", "2", ""};
-		String[] weights = {"5 3", "7", ""};
-		
-		Circuits c = new Circuits();
-		int result1 = c.howLong(dependencies, weights); 
-		System.out.printf("Expected 12, got %d", result1);
-		
-	}
+//	public static void main(String[] args){
+//		String[] dependencies = {"1 2", "2", ""};
+//		String[] weights = {"5 3", "7", ""};
+//		
+//		Circuits c = new Circuits();
+//		int result1 = c.howLong(dependencies, weights); 
+//		System.out.printf("Expected 12, got %d", result1);
+//	}
 	
 }
