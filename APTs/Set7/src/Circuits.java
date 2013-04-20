6500c461bf5d12edbc9a47ecfd71c5d76cf37b58
Matt Dickenson <mcdickenson@gmail.com>
2013-04-20 13:11:03 -0400

    changed method for Circuits, passes local tests

--- APTs/Set7/src/Circuits.java
+++ APTs/Set7/src/Circuits.java
@@ -15,14 +15,29 @@ public class Circuits {
 			}
 		}
 		
+//		for(int[] row : myGraph){
+//			for(int col: row){
+//				System.out.print(col);
+//				System.out.print(",");
+//			}
+//			System.out.println("");
+//		}
 		
-		int[] longestPaths = new int[connects.length];
+		
+//		int[] longestPaths = new int[connects.length];
+//		for(int i=0; i<connects.length; i++){
+//			ArrayList<Integer> visited = new ArrayList<Integer>();
+//			longestPaths[i] = longestFromMe(myGraph, i, 0, visited);
+//		}
+		
+//		return rowMax(longestPaths);
+		boolean[] visited = new boolean[connects.length];
 		for(int i=0; i<connects.length; i++){
-			ArrayList<Integer> visited = new ArrayList<Integer>();
-			longestPaths[i] = longestFromMe(myGraph, i, 0, visited);
+			visited[i] = false;
 		}
 		
-		return rowMax(longestPaths);
+		return longestPath(0, visited, myGraph);
+//		return 0;
 	}
 	
 	public int longestFromMe(int[][] graph, int start, int pathLength, ArrayList<Integer> visited){
@@ -46,7 +61,27 @@ public class Circuits {
 			return pathLength; 
 		}
 		
+	}
+	
+	public int longestPath(int start, boolean[] visited, int[][] graph){
+		int dist = 0;
+		int max = 0;
+		visited[start] = true;
 		
+		int[] row = graph[start];
+		for(int i=0; i<row.length; i++){
+			if(graph[start][i]>0){
+				if(!visited[i]){
+					dist = row[i] + longestPath(i, visited, graph);
+				}
+				if(dist > max){
+					max = dist;
+				}
+			}
+		}
+		
+		visited[start] = false;
+		return max;
 	}
 	
 	
@@ -68,14 +103,14 @@ public class Circuits {
 	}
 	
 	
-//	public static void main(String[] args){
-//		String[] dependencies = {"1 2", "2", ""};
-//		String[] weights = {"5 3", "7", ""};
-//		
-//		Circuits c = new Circuits();
-//		int result1 = c.howLong(dependencies, weights); 
-//		System.out.printf("Expected 12, got %d", result1);
-//		
-//	}
+	public static void main(String[] args){
+		String[] dependencies = {"1 2", "2", ""};
+		String[] weights = {"5 3", "7", ""};
+		
+		Circuits c = new Circuits();
+		int result1 = c.howLong(dependencies, weights); 
+		System.out.printf("Expected 12, got %d", result1);
+		
+	}
 	
 }
