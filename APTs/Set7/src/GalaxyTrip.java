2f51eb752abadcb283daac014ad6063e6e2c2c15
Matt Dickenson <mcdickenson@gmail.com>
2013-04-20 16:49:01 -0400

    GalaxyTrip passes local test

--- APTs/Set7/src/GalaxyTrip.java
+++ APTs/Set7/src/GalaxyTrip.java
@@ -3,90 +3,86 @@ import java.util.ArrayList;
 //import java.util.BitSet.*;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class GalaxyTrip {
-       public int[] possibleValues(String[] dependencies){
-    	   
-    	   // create adjacency matrix
-    	   int[][] myGraph;
-    	   myGraph = new int[dependencies.length][dependencies.length];
-    	   for(int i = 0; i < dependencies.length; i++){
-    		   if(dependencies[i].length()>0){
-    			   String[] adj = dependencies[i].split(" ");
+	public int[] possibleValues(String[] dependencies){
+ 	   
+ 	   // map visited nodes to their parent (or -1)
+ 	   HashMap<Integer, Integer> visited = new HashMap<Integer, Integer>();
+ 	   HashMap<Integer, Integer> graphSizes = new HashMap<Integer, Integer>(); 
+ 	   
+ 	   for(int current=0; current<dependencies.length; current++){
+ 		   String nodesConnected = dependencies[current];
+ 		   
+ 		   // visit all connections and map this node to size of its graph
+ 		   if(!visited.containsKey(current)){
+ 			   int numConnections = 1;
+ 			   
+ 			   for(int j=0; j<nodesConnected.length(); j++){
+ 				   String connection = nodesConnected.substring(j, j+1);
+ 				   if(!connection.equals(" ")){
+ 					   int node = Integer.parseInt(connection);
+ 					   visited.put(node, current);
+ 					   numConnections++;
+ 				   }
+ 			   }
+ 			   graphSizes.put(current, numConnections);
+ 		   }
+ 	   }
+ 	   
+ 	   int[] sizes = new int[graphSizes.keySet().size()];
+ 	   int j=0;
+ 	   for(int key : graphSizes.keySet()){
+ 		   int s = graphSizes.get(key);
+ 		   sizes[j] = s;
+ 		   j++;
+ 	   }
+ 	   
 
-    			   for(int j = 0; j < adj.length; j++){
+ 	   
+ 	   HashSet<Integer> sums = sumArray(sizes);
+ 	   
+ 	   
+ 	   int[] result = new int[sums.size()];
+ 	   int i=0;
+ 	   for(int s: sums){
+ 		   result[i] = s;
+ 		   i++;
+// 		   System.out.println(s);
+ 	   }
+ 	   
+ 	   return result; 
+    }
+    
+	public HashSet<Integer> sumArray(int[] sizes){
+		HashSet<Integer> counts = new HashSet<Integer>();
+		
+		// loop through possible windows
+		for(int window=1; window<=sizes.length; window++){
+			
+			// loop through sizes
+			for(int i=0; i<sizes.length-window+1; i++){
+				int thisWindow = 0;
+				for(int j=i; j<i+window; j++){
+					thisWindow += sizes[j];
+				}
+				counts.add(thisWindow);
+			}
+			// add everything in that window size
+			
+		}
+		
+		return counts;
+	}
+       
+	public static void main(String[] args){
+		GalaxyTrip gt = new GalaxyTrip();
 
-    				   myGraph[i][Integer.parseInt(adj[j])] = 1;
-//    				   myGraph[Integer.parseInt(adj[j])][i] = 1;
-
-    			   }
-    		   }
-    		   myGraph[i][i] = 1; 
-   			}
-    	   for(int[] row: myGraph){
-    		   for(int c: row){
-    			   System.out.print(c);
-    			   System.out.print(",");
-    		   }
-    		   System.out.println();
-    	   }
-    	   
-    	   int[] neighbors = new int[dependencies.length];
-    	   for(int i=0; i<dependencies.length; i++){
-    		   boolean[] visited = new boolean[dependencies.length];
-    		   for(int j=0; j<dependencies.length; j++){
-    			   visited[j] = false;
-    		   }
-    		   neighbors[i] = countNeighbors(i, visited, myGraph);
-    	   }
-    	   
-    	   for(int n: neighbors){
-    		   System.out.println(n);
-    	   }
-    		
-    	   
-    	   int[] result = {1, 2, 3};  
-    	   return result; 
-       }
-          
-       
-       public int countNeighbors(int start, boolean[] visited, int[][] graph){
-   		int dist = 0;
-   		int max = 0;
-   		visited[start] = true;
-   		
-   		int[] row = graph[start];
-   		for(int i=0; i<row.length; i++){
-   			if(graph[start][i]>0){
-   				if(!visited[i]){
-   					dist = row[i] + countNeighbors(i, visited, graph);
-   				}
-   				if(dist > max){
-   					max = dist;
-   				}
-   			}
-   		}
-   		
-   		visited[start] = false;
-   		return max;
-   	}
-       
-//       public int[] buildIntArray(ArrayList<Integer> integers) {
-//    	    int[] ints = new int[integers.size()];
-//    	    int i = 0;
-//    	    for (Integer n : integers) {
-//    	        ints[i++] = n;
-//    	    }
-//    	    return ints;
-//    	}
-       
-       public static void main(String[] args){
-    	   GalaxyTrip gt = new GalaxyTrip();
-    	   
-    	   String[] d1 = {"1 2", "0", "0", ""};
-    	   int[] test1 = {1, 3, 4};
-    	   int[] result1 = gt.possibleValues(d1);
-//    	   if(test1.length==result1.length){ System.out.println("test 1 passes");}
-//    	   else{System.out.println("test 1 fails");}
-       }
+		String[] d1 = {"1 2", "0", "0", ""};
+		int[] test1 = {1, 3, 4};
+		int[] result1 = gt.possibleValues(d1);
+		if(test1.length==result1.length){ System.out.println("test 1 passes");}
+		else{System.out.println("test 1 fails");}
+	}
 }
